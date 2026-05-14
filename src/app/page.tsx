aec2b43309efe95a5efe241f3886fc6978c416e6
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-05-14 19:31:50 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-"use client";
+import { Button } from './_components/ui/button';
 
-import { useState } from "react";
-
-//Server Components
 const Home = () => {
-  const [] = useState();
-  return <h1 className="text-red-500 mb-5 bg-red-400">red text</h1>;
+  return <Button>Click me</Button>;
 };
 
 export default Home;
