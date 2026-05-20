ee2cbc10daa4e922644dd5d5064bbc6209ec90d8
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-05-20 19:50:37 -0300

    feat: button header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image"
 import { Card, CardContent } from "@/components/ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
 
 const Header = () => {
   return (
     <Card>
-      <CardContent>
+      <CardContent className="flex flex-row items-center justify-between p-5">
         <Image
           src="/logo.png"
           alt="Logo da barbearia"
           height={18}
           width={120}
         />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
       </CardContent>
     </Card>
   )
