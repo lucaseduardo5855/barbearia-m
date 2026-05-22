fcb5ad5a54bf01afdfead2f41758839af70fa8b3
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-05-21 21:51:53 -0300

    feat: add BarbershopItem component - recomended

--- src/app/_components/barbershop-item.tsx
+++ src/app/_components/barbershop-item.tsx
@@ -1,11 +1,51 @@
 import { Barbershop } from "@prisma/client"
+import { Card, CardContent } from "./ui/card"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import { Badge } from "./ui/badge"
+import { StarIcon } from "lucide-react"
 
 interface BarbershopItemProps {
   barbershop: Barbershop
 }
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
-  return <h1>{barbershop.name}</h1>
+  return (
+    <Card className="min-w-[159px] overflow-hidden rounded-2xl">
+      <CardContent className="p-0 px-2">
+        {/* IMAGEM */}
+        <div className="relative h-[159px] w-full">
+          <Image
+            alt={barbershop.name}
+            fill
+            className="rounded-2xl object-cover"
+            src={barbershop.imageUrl}
+          />
+
+          <Badge
+            className="absolute left-2 top-2 space-x-1 rounded-xl"
+            variant="secondary"
+          >
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="text-xs font-semibold">5,0</p>
+          </Badge>
+        </div>
+
+        {/* NOME e ENDEREÇO */}
+        <div className="px-1 py-3">
+          <h3 className="overflow-hidden truncate text-ellipsis text-nowrap font-semibold">
+            {barbershop.name}
+          </h3>
+          <p className="overflow-hidden truncate text-sm text-gray-400">
+            {barbershop.address}
+          </p>
+          <Button variant="secondary" className="mt-3 w-full">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default BarbershopItem
