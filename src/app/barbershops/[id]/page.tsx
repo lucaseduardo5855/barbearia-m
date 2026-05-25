497a306f0e69d1a3b51d8621bcf5c0422aeb5494
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-05-24 22:17:21 -0300

    feat: ensure services barbershop are shown on barbershop page

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,8 +1,10 @@
+import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/lib/prisma"
 import { ChevronLeftIcon, StarIcon, MapPinIcon, MenuIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 interface BarbershopPageProps {
   params: {
@@ -16,14 +18,25 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
     where: {
       id: params.id,
     },
+    include: {
+      services: true, // Incluir os serviços oferecidos pela barbearia
+    },
   })
+
+  barbershop?.services
+
+  //se não tiver barbearia, mostrar página de 404
+  if (!barbershop) {
+    return notFound()
+  }
+
   return (
     <div>
       {/* IMAGEM */}
       <div className="relative h-[250px] w-full">
         <Image
-          alt={barbershop?.name || "Foto da Barbearia"}
-          src={barbershop?.imageUrl || "Imagem da Barbearia"}
+          alt={barbershop?.name}
+          src={barbershop?.imageUrl}
           fill
           className="object-cover"
         />
@@ -50,12 +63,12 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
 
       <div className="border-b border-solid p-5">
         <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
-        <div className="mb-2 flex items-center gap-1">
+        <div className="mb-2 flex items-center gap-2">
           <MapPinIcon className="size={18} text-primary" />
           <p className="text-sm text-gray-400">{barbershop?.address}</p>
         </div>
 
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-2">
           <StarIcon className="fill-primary text-primary" />
           <p className="text-sm text-gray-400">5,0 (482 avaliações)</p>
         </div>
@@ -65,6 +78,16 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       <div className="space-y-2 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="justify-text text-sm">{barbershop?.description}</p>
+      </div>
+
+      {/* SERVIÇOS */}
+      <div className="p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3 pt-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   )
