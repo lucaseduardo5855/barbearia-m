b76072a7e81ac3d90af1d438e683b799050f426e
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-05-24 21:29:57 -0300

    feat: create barbershop details dynamic page add page -> button -> bdID

---
+++ src/app/barbershops/[id]/page.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@/app/_components/ui/button"
+import { db } from "@/lib/prisma"
+import { ChevronLeftIcon, StarIcon, MapPinIcon, MenuIcon } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  //chamar bd
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+  return (
+    <div>
+      {/* IMAGEM */}
+      <div className="relative h-[250px] w-full">
+        <Image
+          alt={barbershop?.name || "Foto da Barbearia"}
+          src={barbershop?.imageUrl || "Imagem da Barbearia"}
+          fill
+          className="object-cover"
+        />
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute left-4 top-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute right-4 top-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
+        <div className="mb-2 flex items-center gap-1">
+          <MapPinIcon className="size={18} text-primary" />
+          <p className="text-sm text-gray-400">{barbershop?.address}</p>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <StarIcon className="fill-primary text-primary" />
+          <p className="text-sm text-gray-400">5,0 (482 avaliações)</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopPage
