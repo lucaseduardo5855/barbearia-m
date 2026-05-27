f68eb6df40ecba148cbf5227b45033d416caad0a
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-05-27 20:46:01 -0300

    chore: add next auth setup - Autenticação

--- src/app/_components/sidebar-sheet.tsx
+++ src/app/_components/sidebar-sheet.tsx
@@ -1,15 +1,25 @@
+"use client"
+
 import {
   SheetClose,
   SheetContent,
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { AvatarImage, Avatar } from "./ui/avatar"
 import quickSearchOptions from "../_constants/search"
 import Image from "next/image"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -18,17 +28,44 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      {/* Foto Perfil */}
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      {/* Foto Perfil e Informações do Usuário */}
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="text-lg font-bold">Olá, Realize seu Login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-2 font-bold">
+              <Image
+                alt="Fazer login com o Google"
+                src="/Google.svg"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/*<Avatar>
           <AvatarImage src="/avatar.png" />
         </Avatar>
 
-        {/* Informações do Usuário */}
         <div>
           <p className="font-bold">Lucas Eduardo</p>
           <p className="text-sm text-gray-400">lucas.eduardo@gmail.com</p>
         </div>
+      */}
       </div>
 
       {/* Botões do Menu */}
